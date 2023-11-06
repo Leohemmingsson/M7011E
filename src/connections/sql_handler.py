8045787ac871916a874ca282587e915689d6c381
Leo <leohemmingsson@gmail.com>
2023-11-06 17:26:48 +0100

    token works

--- src/connections/sql_handler.py
+++ src/connections/sql_handler.py
@@ -1,12 +1,43 @@
-from .baseConnection import BaseConnection
+# std
+import os
+
+# pip
+import mysql.connector
 
 
-class SqlHandler(BaseConnection):
-    def get_product_from_id(self, id):
-        ...
+class SqlManager:
+    """
+    Context manager for SQL connections.
+    Makes to load env variables, commit and close connection automatically.
 
-    def add_product(self, product):
-        ...
+    # Usage example:
 
-    def remove_product(self, id):
-        ...
+    with SQLManager() as (cursor, mydb):
+        cursor.execute("SELECT * FROM users")
+        result = cursor.fetchall()
+        print(result)
+
+    """
+
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+            host=os.getenv("SQL_HOST"),
+            user=os.getenv("SQL_USER"),
+            port=os.getenv("SQL_PORT"),
+            password=os.getenv("SQL_PASS"),
+            database=os.getenv("SQL_DB"),
+        )
+
+    def __enter__(self):
+        return (self.mydb.cursor(), self.mydb)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.mydb.commit()
+        self.mydb.close()
+
+
+if __name__ == "__main__":
+    with SqlManager() as (cursor, mydb):
+        cursor.execute("SELECT * FROM users")
+        result = cursor.fetchall()
+        print(result)
--- src/v1/users.py
+++ src/v1/users.py
@@ -1,13 +1,37 @@
-from flask import Blueprint
+# std
+import uuid
+
+# own
+from permissions import token_required, hash_password
+from data_types import User
+
+# pip
+from flask import Blueprint, make_response, request
 
 users_bp = Blueprint("users", __name__)
 
 
-@users_bp.route("/users", endpoint="users")
-def users():
+@users_bp.route("/users", methods=["POST"], endpoint="create_user")
+# @token_required
+def create_user():
+    print("[Warning] Need to check if user is admin, otherwise anyone can create a user, or maybe mail check?")
+
+    data = request.get_json()
+
+    hashed_password = hash_password(data["password"])
+    new_user = User(public_id=str(uuid.uuid4()), username=data["name"], password=hashed_password, type="user")
+
+    print(new_user)
+
+    return make_response("User created", 201)
+
+
+@users_bp.route("/users", methods=["GET"], endpoint="users")
+@token_required
+def users(current_user):
     return "Users"
 
 
-@users_bp.route("/users/<int:id>", endpoint="get_from_id")
-def show():
+@users_bp.route("/users/<int:id>", methods=["GET"], endpoint="get_from_id")
+def get_from_id():
     return f"One user: {id}"
