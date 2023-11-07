aed32b46e5de1d35f9698928bcb8bc6ab8d4246d
Leo <leohemmingsson@gmail.com>
2023-11-07 16:04:08 +0100

    sql works

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import os
 
 # from flask_sqlalchemy import SQLAlchemy
 from shared_models import db
+from dotenv import load_dotenv
 
 # from orm import db
 import v1
@@ -9,7 +11,16 @@ import v1
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+load_dotenv()
+
+USERNAME = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASS")
+HOST = os.getenv("DB_HOST")
+PORT = os.getenv("DB_PORT")
+DATABASE = os.getenv("DB_DATABASE")
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 
 
 db.init_app(app)
