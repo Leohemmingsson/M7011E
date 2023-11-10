99ff439992d20b29b90b4d1a483014dfd6bce5d0
Leo <leohemmingsson@gmail.com>
2023-11-10 11:20:53 +0100

    confirming mail works

--- src/v1/users.py
+++ src/v1/users.py
@@ -1,13 +1,16 @@
 # std
 import uuid
+import os
 
 # own
 from permissions import token_required, hash_password, is_authorized, AuthorizationLevel
+from mail import send_confirmation_email
 from orm import User
 
 
 # pip
 from flask import Blueprint, make_response, request, jsonify
+import yagmail
 
 users_bp = Blueprint("users", __name__)
 
@@ -18,12 +21,17 @@ def post_create_user():
 
     data["password"] = hash_password(data["password"])
     data["public_id"] = str(uuid.uuid4())
-    data["activated"] = True
+    data["activated"] = False
     data["type"] = AuthorizationLevel.CUSTOMER.name
     # data["type"] = AuthorizationLevel.ADMIN.name
     # data["type"] = AuthorizationLevel.SUPERUSER.name
 
-    User.add(**data)
+    if "mail" not in data:
+        return make_response("Mail is missing", 400)
+
+    new_user = User.add(**data)
+
+    send_confirmation_email(new_user)
 
     return make_response("User created", 201)
 
@@ -42,9 +50,15 @@ def get_all_users(current_user):
 
 @users_bp.route("/users/confirm/<string:public_id>", methods=["GET"], endpoint="confirm_mail")
 def confirm_mail(public_id):
-    # sql_activate_user(cursor, public_id)
+    statement = User.public_id == public_id
+    user_to_confirm = User.get_first_where(statement)
 
-    return make_response("User activated", 200)
+    if not user_to_confirm:
+        return make_response("User not found", 404)
+
+    user_to_confirm.update("activated", True)
+
+    return make_response("Mail confirmed", 200)
 
 
 @users_bp.route("/users/<string:username>", methods=["GET"], endpoint="get_user_from_username")
