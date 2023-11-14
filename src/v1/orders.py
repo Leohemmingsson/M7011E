ae6c1dd6e398418a076792527b2378a2acb12655
1inss <linus.hakegard@gmail.com>
2023-11-14 23:23:59 +0100

    added order routes

--- src/v1/orders.py
+++ src/v1/orders.py
@@ -1,19 +1,36 @@
 # own
+from orm import Order
 from permissions import token_required
 
 # pip
-from flask import Blueprint
+from flask import Blueprint, make_response, request, jsonify
 
 orders_bp = Blueprint("orders", __name__)
 
 
-@orders_bp.route("/orders", methods=["GET"], endpoint="create_order")
+@orders_bp.route("/orders", methods=["POST"], endpoint="post_create_order")
+@token_required
+def post_create_order(current_user):  # lägga till authorization
+    data = request.get_json()
+    Order.add(current_user, **data)
+    return make_response("Order created", 201)
+
+
+@orders_bp.route("/orders", methods=["GET"], endpoint="get_all_orders")
 @token_required
 def get_all_orders(current_user):
-    return "Orders"
+    orders = Order.get_all()
+    orders = [one_order.to_dict for one_order in orders]
+    return jsonify(orders)
 
 
-@orders_bp.route("/orders/<int:id>", methods=["GET"], endpoint="get_orders_from_id")
+@orders_bp.route("/orders/<int:id>", methods=["GET"], endpoint="get_order_from_id")
 @token_required
-def get_orders_from_id(current_user, id):
-    return f"One order: {id}"
+def get_order_from_id(current_user, id):
+    statement = Order.id == id
+    order = Order.get_first_where(statement)
+
+    if not order:
+        return make_response("Order not found", 404)
+
+    return jsonify(order.to_dict)
