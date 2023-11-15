bf7ed5ea3254a7813f01f06613938c89ad2733e1
Leo <leohemmingsson@gmail.com>
2023-11-15 19:33:21 +0100

    templating order stuff

--- item_worker/tasks.py
+++ item_worker/tasks.py
@@ -20,7 +20,9 @@ app.conf.task_routes = {
     "scheduler.create_item": {"queue": "item"},
     "scheduler.get_all_items": {"queue": "item"},
     "scheduler.get_item_by_id": {"queue": "item"},
+    "scheduler.get_item_by_name": {"queue": "item"},
     "scheduler.delete_item_by_id": {"queue": "item"},
+    "scheduler.delete_item_by_name": {"queue": "item"},
 }
 
 
@@ -48,6 +50,16 @@ def get_all_items():
     return (all_items, 200)
 
 
+@app.task(queue="item", name="get_item_by_name")
+def get_item_by_name(name):
+    statement = Item.name == name
+    item = Item.get_first_where(statement)
+    if item is None:
+        return ("Item not found", 404)
+
+    return (item.to_dict, 200)
+
+
 @app.task(queue="item", name="get_item_by_id")
 def get_item_by_id(id):
     statement = Item.id == id
@@ -64,3 +76,41 @@ def delete_item_by_id(id):
     Item.delete_where(statement)
 
     return (f"Item {id} deleted", 200)
+
+
+@app.task(queue="item", name="delete_item_by_name")
+def delete_item_by_name(name):
+    statement = Item.name = name
+    Item.delete_where(statement)
+
+    return (f"Item {id} deleted", 200)
+
+
+@app.task(queue="item", name="create_order")
+def create_order(data: dict):
+    raise NotImplementedError("Not implemented yet")
+
+
+@app.task(queue="item", name="add_item_to_order")
+def add_item_to_order(order_id: int, item_id: int, quantity: int):
+    raise NotImplementedError("Not implemented yet")
+
+
+@app.task(queue="item", name="remove_item_from_order")
+def remove_item_from_order(order_id: int, item_id: int, quantity: int):
+    raise NotImplementedError("Not implemented yet")
+
+
+@app.task(queue="item", name="get_all_orders")
+def get_all_orders():
+    raise NotImplementedError("Not implemented yet")
+
+
+@app.task(queue="item", name="get_order_by_id")
+def get_order_by_id(id):
+    raise NotImplementedError("Not implemented yet")
+
+
+@app.task(queue="item", name="delete_order_by_id")
+def delete_order_by_id(id):
+    raise NotImplementedError("Not implemented yet")
