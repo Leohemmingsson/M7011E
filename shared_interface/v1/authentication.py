553f753bd906d41b264bb6eedc4d9bca0fae9f74
1inss <linus.hakegard@gmail.com>
2023-11-16 12:10:14 +0100

    fix

--- shared_interface/v1/authentication.py
+++ shared_interface/v1/authentication.py
@@ -40,36 +40,6 @@ def login():
     if user["activated"] is not True:
         return make_response("User not activated", 401)
 
-    if check_password_hash(auth.password, user["password"]):
-        token = jwt.encode(
-            {"public_id": user["public_id"], "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
-            SECRET_KEY,
-        )
-
-        return jsonify({"token": token})
-
-    return make_response("Could not verify", 401, {"WWW-Authenticate": 'Basic realm="Login required!"'})
-
-
-@authentication_pb.route("/login", endpoint="login")
-def login():
-    """
-    Path to login with basic auth, this returns a token that can be used to access the api.
-    Default TTL for a token is 30 minutes.
-    """
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:
-        return make_response("Could not verify", 401, {"WWW-Authenticate": 'Basic realm="Login required!"'})
-
-    req = get_user_by_username.delay(auth.username)
-    user, status_code = req.get()
-
-    if status_code != 200:
-        return make_response("Could not verify", 401, {"WWW-Authenticate": 'Basic realm="Login required!"'})
-
-    if user["activated"] is not True:
-        return make_response("User not activated", 401)
-
     if not check_password_hash(auth.password, user["password"]):
         return make_response("Could not verify", 401, {"WWW-Authenticate": 'Basic realm="Login required!"'})
 
